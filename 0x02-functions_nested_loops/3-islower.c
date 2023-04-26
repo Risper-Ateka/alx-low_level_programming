db2593ff825b38ed138a6e7110974af57bf50449
risper <nrisperateka@gmail.com>
2023-04-26 08:49:11 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ *_islower - shows 1 if the input is a
+ *lowercase character. Another case, shows
+ *0
+ *
+ *@c: character in ASCII table
+ *
+ *Return: 1 for lower case. 0 for the rest
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
