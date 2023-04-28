929cbe9f87a96d43fbf43f7f6b8a3df6e45605b9
risper <nrisperateka@gmail.com>
2023-04-28 00:02:20 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
   *print_most_numbers - print numbers except 2 and 4
-  *@c: character to be defined
+  *
   *Return: void
   *
   */
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2') || (c == '4'))
+		if !(c == '2') || !(c == '4')
 			_putchar(c);
 	}
 	_putchar('\n');
