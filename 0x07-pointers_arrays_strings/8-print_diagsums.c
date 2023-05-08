1db38c84143a323f67bde685d3a32f89013ef1b0
risper <nrisperateka@gmail.com>
2023-05-08 07:45:47 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (y = size - 1; y >= 0; y--)
 	{
-		sum += a[y * size + (size - y - 1)];
+		sum = a[y * size + (size - y - 1)];
 	}
 	printf("%d", "%d\n", sum1, sum2);
 }
