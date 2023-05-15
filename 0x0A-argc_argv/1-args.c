2d4c5924e5a4bad8a420bb5a96037dc450cedfc1
risper <nrisperateka@gmail.com>
2023-05-15 03:59:22 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - entry point
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: 0 (success)
+  */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
