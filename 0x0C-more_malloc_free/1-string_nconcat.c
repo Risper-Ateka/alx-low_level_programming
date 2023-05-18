3db368bc81a94563a5f91b15c1dea2fb1ce52b6d
risper <nrisperateka@gmail.com>
2023-05-18 14:00:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,8 +12,8 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = 0;
-	unsigned int s2_len = 0;
+	unsigned int l1 = 0;
+	unsigned int l2 = 0;
 	unsigned int a = 0;
 	unsigned int b = 0;
 	char *str;
@@ -22,20 +22,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-	if (n < s2_len)
-		str = malloc((s1_len + n + 1) * sizeof(char));
-	else if (n >= s2_len)
-		str = malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (str == NULL)
+	while (s1 && s1[l1])
+		l1++;
+	while (s2 && s2[l2])
+		l2++;
+	if (n < l2)
+		str = malloc(sizeof(char) * (l1 + n + 1));
+	else
+		str = malloc(sizeof(char) * (l2 + l2 + 1));
+	if (!str)
 		return (NULL);
-	for (a = 0; a < s1_len; a++)
+	while (a < l1)
+	{
 		str[a] = s1[a];
-	for (b = 0; b < s2_len; b++)
-		str[a + b] = s2[b];
-	str[a + b] = '\0';
+		a++;
+	}
+	while (n < l2 && a < (l1 + n))
+		str[a++] = s2[b++];
+	while (n >= l2 && a < (l1 + l2))
+		str[a++] = s2[b++];
+	str[a] = '\0';
 	return (str);
 }
