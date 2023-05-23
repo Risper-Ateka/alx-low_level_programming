bf1a97874742bdce29a5ad6d5f75f87f70e95382
risper <nrisperateka@gmail.com>
2023-05-23 05:21:57 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "(nil)"
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->name = "(nil)"
+		d->name = "(nil)";
 	printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner);
 }
