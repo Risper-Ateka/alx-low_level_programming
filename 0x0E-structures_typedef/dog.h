197c151b691e92820f43d82ff16700d76c6e3b6d
risper <nrisperateka@gmail.com>
2023-05-23 00:41:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MY_STRUCTURE_H
+#define MY_STRUCTURE_H
+
+/**
+  *struct dog - a struct containing the details about a dog
+  *@name: dog's name
+  *@age: dog's age
+  *@owner: dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
