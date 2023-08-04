c12fefcd64254009cf18bbf845b5d1f6ff071c66
Risper <nrisperateka@gmail.com>
2023-08-04 02:55:17 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 (success)
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
